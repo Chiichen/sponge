52041e3bdd92d6f8f5c45bc8a615e41f5956a974
chiichen <chiichen@qq.com>
2024-01-16 00:47:20 +0800

    Lab2: 92% Passed. Some Problem remaining in Lab1

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -6,17 +6,14 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return WrappingInt32{isn + static_cast<uint32_t>(n)}; }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +26,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset = n - isn;                      // We have absolute_seq % INT32_MAX == offset;
+    if (checkpoint > offset) {                      // Consider Overflow
+        const constexpr uint64_t MAX32 = 1l << 32;  // CAN NOT use UINT32_MAX here!
+        uint64_t cur_checkpoint =
+            (checkpoint - offset) + (MAX32 >> 1);  // We construct it as rounding in base INT32-MAX
+        uint64_t wrap_num = cur_checkpoint / MAX32;
+        return wrap_num * MAX32 + offset;
+    } else
+        return offset;
 }
